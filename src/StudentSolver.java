3100e4e61b6223d68886bf7b53c221552dd3fcbc
Teddy Onditi <TeddyOtieno.Onditi@student.csulb.edu>
2021-11-13 13:13:48 -0800

    Pair Class Addition and Main Method

--- src/StudentSolver.java
+++ src/StudentSolver.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
 
 public class StudentSolver {
-
+	public static ArrayList<Pair<Integer,Integer>> solve(int[][] board){
+		ArrayList<Pair<Integer, Integer>> indeces = new ArrayList<Pair<Integer, Integer>>();
+		
+		return indeces; 
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
