4eadb407f610e3cf02830ad2862d3cea710a1eee
Teddy Onditi <TeddyOtieno.Onditi@student.csulb.edu>
2021-11-16 20:23:12 -0800

    successfully done recursion once...
    problem arises if it need to happen more than once...working on it

--- src/StudentSolver.java
+++ src/StudentSolver.java
@@ -35,67 +35,74 @@ public class StudentSolver {
 				// Going to implement restrictions
 				int m = (int) index.first;
 				int n = (int) index.second;
-				if (n == 0) {
-					
-					if (board[m][n]>board[m][n+1]) {
-						exit =true;
-						if (m == 0) {
-							board[m + 1][n] = 0;
-							board[m][n + 1] = 0;
-						} else if (m == rows - 1) {
-							board[m - 1][n] = 0;
-							board[m][n + 1] = 0;
-						} else {
-							board[m - 1][n] = 0;
-							board[m + 1][n] = 0;
-							board[m][n + 1] = 0;
-						}
-					}
-					else {
-						isolatedColumn.remove(indexOfMax);
-					}
-
-				} else if (n == columns - 1) {
-					if(board[m][n]>board[m][n-1]) {
-						exit =true;
+				
+				if (maxValue ==0) {
+					exit =true;
+				}
+				else {
+					if (n == 0) {
 						
-						if (m == 0) {
-							board[m + 1][n] = 0;
-							board[m][n - 1] = 0;
-						} else if (m == rows - 1) {
-							board[m - 1][n] = 0;
-							board[m][n - 1] = 0;
-						} else {
-							board[m - 1][n] = 0;
-							board[m + 1][n] = 0;
-							board[m][n - 1] = 0;
+						if (board[m][n]>board[m][n+1]) {
+							exit =true;
+							if (m == 0) {
+								board[m + 1][n] = 0;
+								board[m][n + 1] = 0;
+							} else if (m == rows - 1) {
+								board[m - 1][n] = 0;
+								board[m][n + 1] = 0;
+							} else {
+								board[m - 1][n] = 0;
+								board[m + 1][n] = 0;
+								board[m][n + 1] = 0;
+							}
+						}
+						else {
+							isolatedColumn.remove(indexOfMax);
+						}
+
+					} else if (n == columns - 1) {
+						if(board[m][n]>board[m][n-1]) {
+							exit =true;
+							
+							if (m == 0) {
+								board[m + 1][n] = 0;
+								board[m][n - 1] = 0;
+							} else if (m == rows - 1) {
+								board[m - 1][n] = 0;
+								board[m][n - 1] = 0;
+							} else {
+								board[m - 1][n] = 0;
+								board[m + 1][n] = 0;
+								board[m][n - 1] = 0;
+							}	
+						}
+						
+						
+					} else {
+						if(board[m][n]>board[m][n+1]) {
+							exit =true;
+							if (m == 0) {
+								board[m + 1][n] = 0;
+								board[m][n + 1] = 0;
+								board[m][n - 1] = 0;
+							} else if (m == rows - 1) {
+								board[m - 1][n] = 0;
+								board[m][n + 1] = 0;
+								board[m][n - 1] = 0;
+							} else {
+								board[m - 1][n] = 0;
+								board[m + 1][n] = 0;
+								board[m][n + 1] = 0;
+								board[m][n - 1] = 0;
+							}
+						}
+						else {
+							isolatedColumn.remove(indexOfMax);
 						}	
 					}
-					
-					
-				} else {
-					if(board[m][n]>board[m][n+1]) {
-						exit =true;
-						if (m == 0) {
-							board[m + 1][n] = 0;
-							board[m][n + 1] = 0;
-							board[m][n - 1] = 0;
-						} else if (m == rows - 1) {
-							board[m - 1][n] = 0;
-							board[m][n + 1] = 0;
-							board[m][n - 1] = 0;
-						} else {
-							board[m - 1][n] = 0;
-							board[m + 1][n] = 0;
-							board[m][n + 1] = 0;
-							board[m][n - 1] = 0;
-						}
-					}
-					else {
-						isolatedColumn.remove(indexOfMax);
-					}	
 				}
-				if (exit ==true) {
+				
+				if (exit ==true && board[m][n]!=0) {
 					indeces.add(index);
 					board[m][n]=0;
 				}
@@ -116,7 +123,9 @@ public class StudentSolver {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				if (j<columns-1) {
-					if (board[i][j+1] != 0) {
+					//This makes sure that the element to the immediate right is not taken up
+					if (board[i][j]!=0 && board[i][j+1] != 0) {//add or for elements above
+						//System.out.println("This:"+ board[i][j]);
 						repeat =true;
 					}
 				}
@@ -126,15 +135,32 @@ public class StudentSolver {
 				}
 			}
 		}
-
+		
+		//if repeat = true..do the call a function to do the program again
+		ArrayList<Pair<Integer, Integer>> reursionIndexes = new ArrayList<Pair<Integer, Integer>>();;
+		if (repeat ==true) {
+			reursionIndexes= solve(board);
+		}
+		for(Pair p: reursionIndexes) {
+			indeces.add(p);
+		}
 		System.out.println("Rows:" + rows);
+		System.out.println("Repeat: " + repeat);
 
 		return indeces;
 	}
 
 	
-	  public static void main(String[] args) { int[][] board ={ {9,94,51,29,76},
-	  {92,68,23,62,42}, {93,64,22,5,51}, {31,17,24,21,49}, {17,45,47,71,35}};
+	  public static void main(String[] args) { 
+		  int[][] board ={ 
+				  {98,38,6}, 
+				  {73,99,35}, 
+				  {26,6,48}, 
+				  {34,78,92},
+				  {21,46,65},
+				  {55,5,66},
+				  {76,99,20}};
+		  
 	  System.out.println(solve(board));
 	  
 	  }
@@ -142,3 +168,12 @@ public class StudentSolver {
 	 
 
 }
+
+/*
+ * {35,90,54,62,62,69}, {89,17,59,13,76,24}, {73,1,57,11,60,34},
+ * {52,94,21,67,9,77}
+ */
+/*
+ * {9,94,51,29,76}, {92,68,23,62,42}, {93,64,22,5,51}, {31,17,24,21,49},
+ * {17,45,47,71,35}}
+ */
